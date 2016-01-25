490e096ae161ae9830b09e57eaa460945597709c
Tamara Cook <tamara.cook@icloud.com>
2016-01-25 03:55:10 +0100

    Tweak calculator controller

--- src/calculator/calculator-controller.js
+++ src/calculator/calculator-controller.js
@@ -3,28 +3,26 @@ export default /*@ngInject*/class CalculatorController{
     this.twoSided = student.twoSided;
     this.oneSided = student.oneSided;
     this.select('twoSample');
-    function watcher(){
-      const data = $scope.$ctrl.data;
-      const selected = $scope.$ctrl.selected;
-      if(selected === 'twoSample' || selected === 'welch'){
-        if(data.m1 && data.m2 && data.n1 && data.n2 && data.s1 && data.s2){return data;}
-      }
-      else if(selected === 'paired' || selected === 'oneSample'){
-        if(data.m && data.n && data.s){return data;}
-      }
-    }
-    $scope.$watch(watcher, (val, oldVal) => {
-      if(val === oldVal){return;}
-      if(!val){
-        this.stats = undefined;
-        return;
-      }
-      this.stats = student.stats(val, this.selected);
-    }, true);
+    $scope.$watch(
+      () => {
+        if(this.form.$valid){return this.data;}
+      },
+      (val, oldVal) => {
+        if(val === oldVal){return;}
+        if(val === undefined){
+          this.stats = undefined;
+        }
+        else{
+          this.stats = student.stats(val, this.selected);
+        }
+      },
+      true
+    );
   }
   select(val){
     this.selected = val;
     this.data = {};
     this.stats = undefined;
+    if(val === 'oneSample' || val === 'paired'){this.data.mu = 0;}
   }
 }
