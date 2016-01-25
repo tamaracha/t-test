f05a937dca5f9795bb2d080797df620c4a1d4dad
Tamara Cook <tamara.cook@icloud.com>
2016-01-25 03:53:23 +0100

    Update calculator directive for the component method

--- src/calculator/index.js
+++ src/calculator/index.js
@@ -1,9 +1,6 @@
 import controller from './calculator-controller';
 import templateUrl from './calculator.jade';
-const directive = /*@ngInject*/function directive(){
-  return {
-    controller: 'CalculatorController as $ctrl',
-    templateUrl
-  };
+export default {
+  controller,
+  templateUrl
 };
-export {controller, directive};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import angular from 'angular';
 import Big from 'big.js';
 import student from './student';
-import * as calculator from './calculator';
+import calculator from './calculator';
+import MathJaxDirective from './math-jax-directive';
 import bigParser from './big-parser';
 
 export default angular.module('calculator', [
@@ -10,16 +11,7 @@ export default angular.module('calculator', [
 ])
 .constant('Big',Big)
 .factory('student', student)
-.controller('CalculatorController', calculator.controller)
-.directive('calculator', calculator.directive)
-.directive('mathJax', /*@ngInject*/function($window){
-  function link(scope, el){
-    $window.MathJax.Hub.Queue(['Typeset', $window.MathJax.Hub, el[0]]);
-  }
-  return {
-    restrict: 'A',
-    link
-  };
-})
+.component('calculator', calculator)
+.directive('mathJax', MathJaxDirective)
 .directive('big', bigParser)
 .name;
